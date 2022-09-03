860f126e0a53f9914a91ebb7de8f201d2d8e824d
Se-myung-Jin <tpaud94@naver.com>
2022-09-03 23:26:38 +0900

    로그인 추가

--- ServerTool/DatabaseLib/Data/IUsersData.cs
+++ ServerTool/DatabaseLib/Data/IUsersData.cs
@@ -2,6 +2,7 @@
 {
     public interface IUsersData
     {
+        Task<Users> SelectUser(Users user);
         Task<bool> InsertUsers(Users user);
     }
 }
--- ServerTool/DatabaseLib/Data/UsersData.cs
+++ ServerTool/DatabaseLib/Data/UsersData.cs
@@ -15,6 +15,15 @@ namespace DatabaseLib
             _db = db;
         }
 
+        public async Task<Users> SelectUser(Users user)
+        {
+            string sql = @"SELECT * FROM dbo.Users WHERE Email = @Email AND Password = @Password;";
+
+            var list = await _db.LoadData<Users, dynamic>(sql, new Users { Email = user.Email, Password = user.Password });
+
+            return list.FirstOrDefault();
+        }
+
         public async Task<bool> InsertUsers(Users user)
         {
             string sql = @"insert into dbo.Users (Email, Password) values (@Email, @Password);";
